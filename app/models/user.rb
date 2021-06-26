7ea2e0f6d56d20a3e3cf0767dc7e421c4c362c6f
依田尚悟 <>
2021-06-26 15:47:02 +0900

    rubocop の実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,9 +43,9 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :message_likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy, inverse_of: true
   has_many :followings, through: :active_relationships, source: :follower
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy, inverse_of: true
   has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
