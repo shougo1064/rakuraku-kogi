d03f75eabdbdf1ac55a0eb4c108f6a31a7fb2471
依田尚悟 <>
2021-06-26 20:38:19 +0900

    todo リストに関する API のルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
+      namespace :list do
+        resources :homeworks
+      end
+
     end
   end
 end
