df00d301c73d81d7d4895e4362a9661145a9d23a
依田尚悟 <>
2021-06-27 20:10:37 +0900

    課題作成の実装

--- spec/requests/api/v1/list/homeworks_spec.rb
+++ spec/requests/api/v1/list/homeworks_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Api::V1::List::Homeworks", type: :request do
     context "存在する自分の課題の id を指定したとき" do
       let(:homework_id) { homework.id }
       let(:homework) { create(:homework) }
-      fit "指定した id の課題の詳細が取得できる" do
+      it "指定した id の課題の詳細が取得できる" do
         subject
         res = JSON.parse(response.body)
         expect(response).to have_http_status(:ok)
@@ -45,8 +45,35 @@ RSpec.describe "Api::V1::List::Homeworks", type: :request do
 
     context "存在しない課題の id を指定したとき" do
       let(:homework_id) { 100000 }
-      fit "課題の詳細が取得できない" do
+      it "課題の詳細が取得できない" do
         expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe "POST /api/v1/list/homeworks" do
+    subject { post(api_v1_list_homeworks_path, params: params) }
+    context "適切なパラメータを送信したとき" do
+      let(:params) { { homework: attributes_for(:homework) } }
+      let(:current_user) { create(:user) }
+      before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+      it "課題の作成ができる"do
+      expect { subject }.to change { Homework.count }.by(1)
+      res = JSON.parse(response.body)
+      expect(res["title"]).to eq params[:homework][:title]
+      expect(res["body"]).to eq params[:homework][:body]
+      expect(res["action"]).to be_present
+      expect(res["deadline"]).to be_present
+      expect(res["user"]["id"]).to eq current_user.id
+      expect(response).to have_http_status(200)
+      end
+    end
+    context "不適切なパラメータを送信したとき" do
+      let(:params) { attributes_for(:homework) }
+      let(:current_user) { create(:user) }
+      before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+      it "課題の作成に失敗する" do
+        expect { subject }.to raise_error(ActionController::ParameterMissing)
       end
     end
   end
